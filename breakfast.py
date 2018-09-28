4593f2f24976f162129da24eb06e618213dc093a
Abhishek Chakraborty <deadbeatfour@gmail.com>
2018-09-29 01:07:04 +0530

    Initial commit
    Added some food descriptions for the environment

---
+++ breakfast.py
@@ -0,0 +1,19 @@
+# Save different parts of the breakfast meal in ascending order of preference [lower is better] only for testing purposes
+
+basic = {
+    0: "Idli",
+    1: "Pav Bhaji",
+    2: "Poha",
+    3: "Masala Dosa",
+    4: "Uthappam",
+    5: "Sambhar Vada",
+    6: "Maggi",
+    7: "Alu Pyaaz Parantha"
+}
+
+additional = {
+    0: "Jalebi",
+    1: "Sprouts",
+    2: "Banana",
+    3: "Apple",
+}
---
+++ lunch.py
@@ -0,0 +1,36 @@
+# Save different parts of the lunch meal in ascending order of preference [lower is better] only for testing purposes
+
+dal = { 
+    0: "Arhar",
+    1: "Masoor",
+    2: "Moong",
+    3: "Dal Fry",
+    4: "Dal Tadka",
+    5: "Chana Masala"
+}
+
+rice = {
+    0: "Onion Rice",
+    1: "Chinese Rice",
+    2: "Soyabean Rice",
+    3: "Tomato Rice",
+    4: "Plain Rice"
+}
+
+sabzi = {
+    0: "Hari Kaddu",
+    1: "Besan Pyaaz Pakoda",
+    2: "Alu Gobhi",
+    3: "Bhindi Masala",
+    4: "Lauki Sabzi",
+    5: "Alu Matar",
+    6: "Alu Pata Gobhi",
+    7: "Baingan"
+}
+
+sweets = {
+    0: "Fruit Chat",
+    1: "Papaya",
+    2: "Banana",
+    3: "Apple"
+}
